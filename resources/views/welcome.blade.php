7b04675a0f1587561cb4efd7a9501e448d154601
Carlos Silveira <carlos.silveira@justia.com>
2020-08-05 17:45:29 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
         <div class="panel panel-default">
             <div class="panel-heading"><strong><a name="calcula">Calcula tu Finiquito</a></strong></div>
-            <form action="" method="POST">
+            <form action="">
                 <table class="table">
                     <tbody>
                         <tr>
